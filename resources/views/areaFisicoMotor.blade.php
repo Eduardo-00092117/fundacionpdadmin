4206c4d4730e373bb8e9969a7b04108c66a8bcfa
Alex7498-SV <00068817@uca.edu.sv>
2021-08-04 10:24:45 -0600

    Corrigiendo imagen del area fisico motor

--- resources/views/areaFisicoMotor.blade.php
+++ resources/views/areaFisicoMotor.blade.php
@@ -12,7 +12,8 @@
                 </div>
             </div>
             <div class="col-md">
-                <img src="{{URL::asset('img/areaFisicoMotor/EDITADO-42.jpg')}}">
+                <!--<img src="{{URL::asset('img/areaFisicoMotor/EDITADO-42.jpg')}}">-->
+                <img src="img/{{$area[1]->url_imagen}}" alt="">
             </div>
         </div>
     </div>
@@ -20,7 +21,7 @@
     <div id="moviles">
         <div class="col etapaInicial">
             <div class="col-md">
-                <img src="{{URL::asset('img/areaFisicoMotor/EDITADO-42.jpg')}}">
+                <img src="img/{{$area[1]->url_imagen}}" alt="">
             </div>
             <div class="col-md" id="aFisico">
                 <div>
